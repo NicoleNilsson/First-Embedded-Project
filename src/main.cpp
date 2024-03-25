2957d4cf9280939607a7cd221b8bd240a9bec919
NicoleNilsson <nicole-nilsson@outlook.com>
2024-03-25 12:18:54 +0100

    button seems to work

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,29 @@
 #include <util/delay.h>
 #include <stdio.h>
 
-#define BIT_SET(byte, nbit) ((byte) |= (1<<(nbit)))
-#define BIT_CLEAR(byte ,nbit) ((byte) &= ~(1<<(nbit)))
+#define BIT_SET(byte, nbit) ((byte) |= (1<<(nbit))) //set bit on byte as 1
+#define BIT_CLEAR(byte ,nbit) ((byte) &= ~(1<<(nbit))) //set bit on byte as 0
+#define BIT_CHECK(byte, nbit) ((byte) & (1<<(nbit))) //check if bit on byte is 1
 
-const uint8_t LED_PIN = 0; //define led pin
+const uint8_t LED_PIN = 0; //define led pin (will be used on port B aka pin 8 on freenove)
+const uint8_t BUTTON_PIN = 1; //define led pin (will be used on port B aka 9 on freenove)
+bool LED_ON = false;
 
 int main(void){
   //setup
-  BIT_SET(DDRB, LED_PIN);
+  BIT_CLEAR(DDRB, BUTTON_PIN); //clear pin on DDRB (set as 0) to configure as input
+  BIT_SET(PORTB, BUTTON_PIN); //set pin on PORTB as 1 to activate pull-up resistor
   //main loop
   while(1){
-    _delay_ms(1000); //wait 500ms
-    BIT_SET(PORTB, LED_PIN); //set pin 0 on port B (aka freenove pin 8) as output and turn led on
-    _delay_ms(1000); //wait 500ms
-    BIT_CLEAR(PORTB, LED_PIN); //clear pin 0 on port B and turn led off
+    _delay_ms(100);
+    if(BIT_CHECK(PINB, BUTTON_PIN) == 0){
+      LED_ON = !LED_ON;
+      if(LED_ON == true){
+        BIT_SET(PORTB, LED_PIN); //set pin 0 on port B (aka freenove pin 8) as 1 and turn led on
+      }else if(LED_ON == false){
+        BIT_CLEAR(PORTB, LED_PIN); //clear pin 0 on port B and turn led off
+      }
+    }
   }
 
   return 0;
