9e0dc9c793ce7a784feb4f35fe8a7b74ed5fe39c
NicoleNilsson <nicole-nilsson@outlook.com>
2024-03-27 12:04:28 +0100

    bug fix, button now working with uart

--- src/main.cpp
+++ src/main.cpp
@@ -14,38 +14,54 @@
 const uint8_t LED_PIN = 0; //define led pin (will be used on port B, aka pin 8 on freenove)
 const uint8_t BUTTON_PIN = 1; //define led pin (will be used on port B, aka 9 on freenove)
 
-void uart_transmit_char (unsigned char data){
+
+void pollButton(void){
+  _delay_ms(100);
+  if(BUTTON_PRESSED(PINB, BUTTON_PIN)){ 
+    // LED_ON = !LED_ON;
+    // if(LED_ON == true){
+    //   BIT_SET(PORTB, LED_PIN); //set pin 0 on port B as 1 and turn led on
+    // }else if(LED_ON == false){
+    //   BIT_CLEAR(PORTB, LED_PIN); //clear pin 0 on port B and turn led off
+    // }
+    BIT_FLIP(PORTB, LED_PIN);
+  }
+}
+
+void transmitChar(unsigned char data){
   // wait for transmit buffer to be empty
   while(!(UCSR0A & (1 << UDRE0)));
   // load data into transmit register
   UDR0 = data;
 }
 
-char uart_get_char (void){
+char getChar(void){
   // wait for data
-  while (!(UCSR0A & (1 << RXC0)));
+  while (!(UCSR0A & (1 << RXC0))){
+    pollButton();
+  }
   // return data
   return UDR0;
 }
 
-void uart_transmit_string(const char *str) {
+void transmitString(const char *str) {
   //transmit all characters in string
   while (*str) {
-    uart_transmit_char(*str++);
+    transmitChar(*str++);
   }
 }
 
-void uart_receive_string(char *buffer, uint8_t max_length) {
+void getString(char *buffer, uint8_t maxLength) {
   uint8_t i = 0;
-  char received_char;
+  char inputChar;
   
   // read until newline or until buffer is full
-  while (i < max_length - 1) {
-    received_char = uart_get_char();
-    if (received_char == '\n') {
+  while (i < maxLength - 1) {
+    inputChar = getChar();
+    if (inputChar == '\n') {
       break;
     }
-    buffer[i++] = received_char;
+    buffer[i++] = inputChar;
   }
   buffer[i] = '\0'; // null-terminate the string to know where string ends
 }
@@ -56,36 +72,25 @@ int main(void){
   BIT_SET(PORTB, BUTTON_PIN); //set pin on PORTB as 1 to activate pull-up resistor
   BIT_SET(DDRB, LED_PIN); //set pin on DDRB as 1 to configure pin as output
 
-  //baud=9600, frameformat=8N1
-  uint16_t baud = 9600;
-  uint16_t baud_setting = (F_CPU / 4 / baud - 1) / 2;
+  //baud rate=9600, frameformat=8N1
+  uint16_t baudRate = 9600;
+  uint16_t baudSetting = (F_CPU / 4 / baudRate - 1) / 2;
   UCSR0A = 1 << U2X0; //double speed mode
-  // assign the baud_setting , a.k.a . ubrr (USART Baud Rate Register)
-  UBRR0H = baud_setting >> 8; 
-  UBRR0L = baud_setting;
+  // assign the baudSetting , a.k.a . ubrr (USART baud Rate Register)
+  UBRR0H = baudSetting >> 8; 
+  UBRR0L = baudSetting;
   //set frameformat
   UCSR0C = SERIAL_8N1;
   //enable Rx, Tx
   UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
 
-  char received_string[50]; //what size to use here? maybe implement safety somehow
+  char inputString[50]; //what size to use here? maybe implement safety somehow
 
   //main loop
   while(1){
-    _delay_ms(100);
-    if(BUTTON_PRESSED(PINB, BUTTON_PIN)){ 
-      // LED_ON = !LED_ON;
-      // if(LED_ON == true){
-      //   BIT_SET(PORTB, LED_PIN); //set pin 0 on port B as 1 and turn led on
-      // }else if(LED_ON == false){
-      //   BIT_CLEAR(PORTB, LED_PIN); //clear pin 0 on port B and turn led off
-      // }
-      BIT_FLIP(PORTB, LED_PIN);
-    }
-
-    uart_receive_string(received_string, sizeof(received_string)); //makes button stop working
-    uart_transmit_string(received_string);
-    uart_transmit_string("\n"); // Add newline for readability
+    getString(inputString, sizeof(inputString)); //stör ut knappen
+    transmitString(inputString);
+    transmitString("\n");
   }
 
   return 0;
