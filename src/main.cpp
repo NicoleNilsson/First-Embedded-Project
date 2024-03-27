6a40dad9270696a054bc9801f012a5deb77ac537
NicoleNilsson <nicole-nilsson@outlook.com>
2024-03-27 11:51:31 +0100

    uart working but not button

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,66 @@
 
 #define BUTTON_PRESSED(byte, nbit) (BIT_CHECK(byte, nbit) == 0) //checking input pin. 0 = button pressed
 
+#define SERIAL_8N1 0x06
+
 const uint8_t LED_PIN = 0; //define led pin (will be used on port B, aka pin 8 on freenove)
 const uint8_t BUTTON_PIN = 1; //define led pin (will be used on port B, aka 9 on freenove)
-// bool LED_ON = false;
+
+void uart_transmit_char (unsigned char data){
+  // wait for transmit buffer to be empty
+  while(!(UCSR0A & (1 << UDRE0)));
+  // load data into transmit register
+  UDR0 = data;
+}
+
+char uart_get_char (void){
+  // wait for data
+  while (!(UCSR0A & (1 << RXC0)));
+  // return data
+  return UDR0;
+}
+
+void uart_transmit_string(const char *str) {
+  //transmit all characters in string
+  while (*str) {
+    uart_transmit_char(*str++);
+  }
+}
+
+void uart_receive_string(char *buffer, uint8_t max_length) {
+  uint8_t i = 0;
+  char received_char;
+  
+  // read until newline or until buffer is full
+  while (i < max_length - 1) {
+    received_char = uart_get_char();
+    if (received_char == '\n') {
+      break;
+    }
+    buffer[i++] = received_char;
+  }
+  buffer[i] = '\0'; // null-terminate the string to know where string ends
+}
 
 int main(void){
   //setup
   BIT_CLEAR(DDRB, BUTTON_PIN); //set pin on DDRB as 0 to configure pin as input
   BIT_SET(PORTB, BUTTON_PIN); //set pin on PORTB as 1 to activate pull-up resistor
   BIT_SET(DDRB, LED_PIN); //set pin on DDRB as 1 to configure pin as output
+
+  //baud=9600, frameformat=8N1
+  uint16_t baud = 9600;
+  uint16_t baud_setting = (F_CPU / 4 / baud - 1) / 2;
+  UCSR0A = 1 << U2X0; //double speed mode
+  // assign the baud_setting , a.k.a . ubrr (USART Baud Rate Register)
+  UBRR0H = baud_setting >> 8; 
+  UBRR0L = baud_setting;
+  //set frameformat
+  UCSR0C = SERIAL_8N1;
+  //enable Rx, Tx
+  UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
+
+  char received_string[50]; //what size to use here? maybe implement safety somehow
 
   //main loop
   while(1){
@@ -31,6 +82,10 @@ int main(void){
       // }
       BIT_FLIP(PORTB, LED_PIN);
     }
+
+    uart_receive_string(received_string, sizeof(received_string)); //makes button stop working
+    uart_transmit_string(received_string);
+    uart_transmit_string("\n"); // Add newline for readability
   }
 
   return 0;
